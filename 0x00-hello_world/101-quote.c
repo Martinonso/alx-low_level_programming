aea3abdd605d9e39dcd7fc644d48c50b31ac4ef8
Martin <martinellyck@gmail.com>
2022-09-10 15:10:56 -0700

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,7 @@
  * Return: 1 if success.
  */
 int main(void)                                                                                                                  
-
 {          
-
-fprintf(stderr, "Anything");
 
 	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);   
 return (1);
