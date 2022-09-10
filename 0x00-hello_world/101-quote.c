eb80f46557a6c0713d6d70768f0e9488449a2fb9
Martin <martinellyck@gmail.com>
2022-09-10 14:46:27 -0700

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,14 @@
   main - Prints out the last part of a quote in the standard error.
  * Return: 1 if success.
  */
-int main(void)                                                                                                               
-{                                                                                                                               
+int main(void)
 
-	        write(2, "and that piece of art is useful - Dora Korpar, 2015-1-19\n", 59);  
-return (1);
+{
+
+	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, ch, sizeof(ch));
+
+	return (1);
+
 }
