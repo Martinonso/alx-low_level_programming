a9fef00bbfa1979741283c89ff61f782f7eeb98b
Martin <martinellyck@gmail.com>
2022-09-10 18:47:59 -0700

    comb

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,14 +22,12 @@ int main(void)
 	int c;
 	int d;
 	int e = 0;
-
 	while (e < 10)
 	{
 		d = 0;
 		while (d < 10)
 		{
 			c = 0;
-		
 			while (c < 10)
 			{
 				if (c != d && d != e && e < d && d < c)
