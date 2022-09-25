4fcb78eafe282630d754551af13801c1eaed6016
Martin <martinellyck@gmail.com>
2022-09-24 23:43:13 -0700

    magic

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,42 +1,26 @@
 #include "main.h"
 
+
 /**
- * print_number - print integer
+ * print_number - prints an integer.
+ * @n: integer
  * Return: void
- * @n: number
  */
 
 void print_number(int n)
 {
-	unsigned int size = n;
-	int digits = 0;
-	int i;
+	int divisor = 1, i, resp;
 
 	if (n < 0)
-		size = -n;
-	if (n == 0)
-		digits = 1;
-	while (size >= 1)
 	{
-		size = size / 10;
-		digits++;
+		_putchar('-');
+		n *= -1;
 	}
-	for (i = 0; i < digits; i++)
+	for (i = 0; n / divisor > 9; i++, divisor *= 10);
+
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
 	{
-		int pow = 1;
-		int j;
-		int d;
-		for (j = 0; j < digits - i - 1; j++)
-		{
-			pow = pow * 10;
-		}
-		d = ((n / pow) % 10);
-		if (n < 0)
-		{
-			d = -d;
-			if (i == 0)
-				_putchar(45);
-		}
-		_putchar(48 + d);
+		resp = n / divisor;
+		_putchar('0' + resp);
 	}
 }
