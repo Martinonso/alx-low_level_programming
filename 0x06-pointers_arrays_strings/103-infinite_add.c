772000a18408dcfbdcaab5363fd67d1cf641f232
Martin <martinellyck@gmail.com>
2022-09-25 16:12:04 -0700

    print buffer

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,37 +11,41 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, l, m, n;
+	int i = 0, j = 0, k = 0;
+	int sum = 0;
+	int tens = 0;
+	int begin = 0;
+	int swap = 0;
 
-	for (i = 0; n1[i]; i++);
-	
-	for (j = 0; n2[j]; j++);
-	
-	if (i > size_r || j > size_r)
+	while (n1[i] != 0)/* A */
+		i++;
+	while (n2[j] != 0)
+		j++;
+	i--;/* C */
+	j--;
+	if (i > size_r || j > size_r)/* D */
 		return (0);
-	m = 0;
-	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
+	for ( ; k < size_r; i--, j--, k++)/* E */
 	{
-		n = m;
-		if (i >= 0)
-			n += n1[i] - '0';
+		sum = tens;
+		if (i >= 0)/* F */
+			sum += n1[i] - '0';
 		if (j >= 0)
-			n += n2[j] - '0';
-		if (i < 0 && j < 0 && n == 0)
-		{
+			sum += n2[j] - '0';
+		if (i < 0 && j < 0 && sum == 0)/* G */
 			break;
-		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
+		tens = sum / 10;/* H */
+		r[k] = sum % 10 + '0';
 	}
-	r[k] = '\0';
-	if (i >= 0 || j >= 0 || m)
+	if (i >= 0 || j >= 0 || sum > 0)/* J */
 		return (0);
-	for (k -= 1, l = 0; l < k; k--, l++)
+	r[k] = '\0';/* K */
+	k--;
+	for ( ; begin < k; k--, begin++)/* I */
 	{
-		m = r[k];
-		r[k] = r[l];
-		r[l] = m;
+		swap = r[k];
+		r[k] = r[begin];
+		r[begin] = swap;
 	}
 	return (r);
 }
