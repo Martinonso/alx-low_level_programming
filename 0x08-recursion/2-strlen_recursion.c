82d28ca3498c80e6f2570e6810b58a87831a7ac9
Martin <martinellyck@gmail.com>
2022-09-28 11:42:35 -0700

    palidrome

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,4 +1,4 @@
-#include "mainn.h"
+#include "main.h"
 
 /**
  * _strlen_recursion - function that returns the length of a string
@@ -8,8 +8,9 @@
 
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	s++;
-	return (1 + _strlen_recursion(s));
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
 }
