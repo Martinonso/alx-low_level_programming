a759b9a4072899f0b5aef052f6e492603ec5f3e0
Martin <martinellyck@gmail.com>
2022-10-04 17:03:31 -0700

    array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,10 +7,9 @@
  * Return: char
  */
 
-/**char *create_array(unsigned int size, char c)
+char *create_array(unsigned int size, char c)
 {
 	char *ar;
-
 	unsigned int i = 0;
 
 	if (size != 0)
@@ -25,15 +24,4 @@
 		i += 1;
 	}
 	return (ar);
-*/ }
-char *create_array(unsigned int size, char c)
-{
-	unsigned int i;
-
-	if (size == 0)
-		return (NULL);
- 	char *s = (char*)malloc(size * sizeof(char));
-	for (i = 0; i < size; i++)
-		s[i] = c;
-	return (s);
 }
