bcddb2a132848b101ad4d9e41d2087cd6d891467
Martinonso <martinellyck@gmail.com>
2023-05-23 17:15:21 -0700

    lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,22 +1,21 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
- * print_dlistint - function that prints all the elements of a dlistint_t list
- * @h: header pointer
- * Return: number of nodes
+ * print_dlistint - prints all the elements of a dlistint_t list.
+ * @h: head of doubly linked list
+ *
+ * Return: the number of nodes
  */
-
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t cont = 0;
+	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		cont++;
+		count++;
 	}
-	return (cont);
+	return (count);
 }
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,22 +1,20 @@
-#include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
 
 /**
- * dlistint_len - function that returns the number of elements in a linked
- * list_t list
- * @h: h pointer
- * Return: number of elements
+ * dlistint_len - returns the number of elements in a linked dlistint_t list.
+ * @h: head of doubly linked list
+ *
+ * Return: number of nodes
  */
-
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t cont = 0;
+	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		h = h->next;
-		cont++;
+		count++;
 	}
-	return (cont);
+	return (count);
 }
